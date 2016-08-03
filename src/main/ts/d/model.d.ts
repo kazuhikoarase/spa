7ee6f762dfae3f0a81893f242525f2b743455ae8
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2016-08-03 20:49:41 +0900

    commit

---
+++ src/main/ts/d/model.d.ts
@@ -0,0 +1,34 @@
+// 自動生成されたファイルです。 編集しないでください。
+
+declare namespace myapp.model {
+
+/**
+ * Test
+ * @author Apache Ant
+ */
+interface TestModel
+{
+
+  /**
+   * param a
+   */
+  a : string;
+
+  /**
+   * param b
+   */
+  b : string;
+
+  /**
+   * operator
+   */
+  ope : string;
+
+  /**
+   * result
+   */
+  res : string;
+
+}
+
+}
